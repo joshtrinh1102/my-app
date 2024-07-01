193e18e74b5f4a1b7b3a3809b50368d92ec169e4
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-30 22:12:32 -0400

    Add personal information section with styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+const user = {
+  name: "Josh Trinh",
+  imageUrl:"https://course-api.com/react-store-products",
+  imageSize :90,
+  linkedIn : "https://www.linkedin.com/in/longtrinhjosh/",
+  email : "longtrinh@usf.edu",
+  bio :"Dedicated and adaptable rising senior at the University of South Florida with a focus on Business Analytics and Information Systems. Adaptability, hardworking and interpersonal skills demonstrated through data analysis projects and extracurricular activities. Eager to leverage technical knowledge and interpersonal skills in professional development"
+ }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Profile(){
+  return(<>
+    <h1>{user.name}</h1>
+    <p>{user.email}/{user.linkedIn}</p>
+    <h3>Biography:</h3>
+    <p>{user.bio}</p>
+    <img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'Photo of ' + user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize
+      }}
+      />
+    
+    </>)
 }
-
-export default App;
